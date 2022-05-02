b0e5ddbd92e4f371ffb20a68dca8ec885498d9ce
kritika518 <86811030+kritika518@users.noreply.github.com>
2022-05-01 20:20:39 -0500

    Add files via upload

---
+++ 97.py
@@ -0,0 +1,8 @@
+import random module()
+random.randint(0,10)
+while chances <5:
+if guess == number:
+    print("Congratulation YOU WON!!")
+    break
+if not chances <5
+print("YOU LOSE!! The number is",number)
